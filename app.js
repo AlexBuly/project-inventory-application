9e26f67b073c56349ed645eb270e0393cb2741b5
Alex Buly <asbuly@icloud.com>
2025-10-15 15:48:52 -0400

    Add index route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
-app.set("view engine", "ejs");
 const app = express();
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
+const indexRouter = require("./routes/indexRouter");
+
+app.use("/", indexRouter);
 
 app.listen(PORT, () =>
   console.log(`App running at http://localhost:${PORT}`)
