0989010e7ed4c0120b4d12760e6ab884168beea8
Alex Buly <asbuly@icloud.com>
2025-10-27 16:18:39 -0400

    Update instruments and accessories route

--- controllers/accessoriesController.js
+++ controllers/accessoriesController.js
@@ -1,5 +1,15 @@
+const pool = require("../db/pool");
+
 const getAccessoriesController = async (req, res) => {
-    res.render("accessories", { title: "Accessories" });
+    try {
+        const { rows: category }  = await pool.query(
+            "SELECT * FROM category WHERE id = 2 ORDER BY id ASC;"
+        )
+        res.render("accessories", { title: "Accessories", category});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error retrieving table");
+    }
 };
 
 module.exports = { getAccessoriesController }
--- db/populatedb.js
+++ db/populatedb.js
@@ -16,11 +16,25 @@ async function populate() {
         await pool.query (
             `
                 INSERT INTO category (categoryName)
-                VALUES ('Instruments');
+                VALUES ('Instruments'),
+                       ('Accessories');
             `
         );
+        await pool.query(
+            `
+                CREATE TABLE IF NOT EXISTS item (
+                    item_id SERIAL PRIMARY KEY,
+                    item_name TEXT,
+                    description TEXT NOT NULL,
+                    category_id INT,
+                    FOREIGN KEY (category_id) REFERENCES category(id),
+                    item_price DECIMAL(10, 2) NOT NULL
+                );
+            `
+        )
     } catch (err) {
-        console.error("Error populating DB:", err);
+         console.error("Error populating DB:", err);
+
     } finally {
         await pool.end();
         console.log("Database populated successfully!");
