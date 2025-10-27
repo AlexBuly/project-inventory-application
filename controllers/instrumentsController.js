c6b247ca218b431022e0075d892d58b136b94cc0
Alex Buly <asbuly@icloud.com>
2025-10-26 20:04:39 -0400

    Add getInstruments query

--- controllers/instrumentsController.js
+++ controllers/instrumentsController.js
@@ -1,5 +1,15 @@
+const pool = require("../db/pool");
+
 const getInstrumentsController = async (req, res) => {
-    res.render("instruments", { title: "Intruments" });
+    try {
+        const { rows: category }  = await pool.query(
+            "SELECT * FROM category ORDER BY id ASC;"
+        )
+        res.render("instruments", { title: "Intruments", category });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error retrieving table");
+    }
 };
 
-module.exports = { getInstrumentsController }
+module.exports = { getInstrumentsController };
