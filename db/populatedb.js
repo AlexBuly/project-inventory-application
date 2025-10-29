82735d0a1d570ac0dfc0a115d963697b038af663
Alex Buly <asbuly@icloud.com>
2025-10-29 14:46:46 -0400

    Insert items on load

--- db/populatedb.js
+++ db/populatedb.js
@@ -31,6 +31,14 @@ async function populate() {
                     item_price DECIMAL(10, 2) NOT NULL
                 );
             `
+        );
+
+        await pool.query(
+            `
+                INSERT INTO item (item_name, description, category_id, item_price)
+                VALUES ('Alto Saxophone', 'A used alto saxophone', 1, 750.00);
+                VALUES ('Alto Saxophone Reed', 'Vandoren Alto Saxophone Traditional Reeds', 2, 3.99);
+            `
         )
     } catch (err) {
          console.error("Error populating DB:", err);
