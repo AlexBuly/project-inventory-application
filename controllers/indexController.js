c78494cf16c2d3a97d3d6275658fd21779b9f56c
Alex Buly <asbuly@icloud.com>
2025-11-12 15:09:28 -0500

    Display all items on index page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,22 @@
+const pool = require("../db/pool");
+
 const getIndexController = async(req, res) => {
-    res.render("index", { title: "Home" });
+    try {
+        const { rows: items } = await pool.query(`
+            SELECT item.*, category.categoryname
+            FROM item
+            INNER JOIN category ON item.category_id = category.id
+            ORDER BY item.item_id ASC;
+    `);
+        res.render("index", {
+        title: "Home",
+        items,
+        categories: res.locals.categories || [],
+        });
+    } catch (err) {
+        console.error("Error fetching all items:", err);
+        res.status(500).send("Error retrieving all items");
+    }
 };
 
 module.exports = { getIndexController }
